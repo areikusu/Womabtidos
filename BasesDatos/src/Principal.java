5b04d46f5130310741d25a49ce1bd17dad4b844c
areikusu <aleix.casanova@gmail.com>
2014-05-07 16:16:41 +0200

    Bueno

--- BasesDatos/src/Principal.java
+++ BasesDatos/src/Principal.java
@@ -4,72 +4,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- * 
- */
 
-/**
- * @author Aleix
- *
- */
 public class Principal {
-
-	/**
-	 * 
-	 */
-	static Connection con = null;
-	public Principal() {
-		// TODO Auto-generated constructor stub
-	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try{
-			Class.forName("java.sql.driver");
-		}
-		catch(ClassNotFoundException cnfe){
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException cnfe){
 			cnfe.printStackTrace();
 		}
-		try{
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/educa?user=root&password=tonphp");
-		} 
-		catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}	
-	//creamos statement para hacer consultas
-	Statement stmt;
-	ResultSet resulConsulta1 = null;
-	try{
 		
-		stmt = con.createStatement();
-		resulConsulta1 = stmt.executeQuery(consulta1);
-		//obtenemos resultados
-		if(resulConnsulta1!=null){
-			while(resulConsulta1.next()){
-				System.out.println(resulConsulta1.getString(1));
-				System.out.println(resulConsulta1.getString("Enombre"));
-			}
+		Connection conexion = null;
+		try{
+		conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1/educa", "root", "tonphp");
+		}catch (SQLException e){
+			e.printStackTrace();
 		}
 		
-	}catch{
+		Statement stat;
+		ResultSet resultadoConsulta = null;
+		try {
+			String consulta1 = "SELECT * FROM PERSONAL";
+			stat = conexion.createStatement();
+			resultadoConsulta = stat.executeQuery(consulta1);
+			if(resultadoConsulta!=null){
+				while(resultadoConsulta.next()){
+					//System.out.println(resultadoConsulta.getString(1));
+					System.out.println(resultadoConsulta.getString("Enombre"));
+					System.out.println("\t"+resultadoConsulta.getString("Esueldo"));
+					System.out.println("\t"+resultadoConsulta.getString("cargo"));
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-	}
-	finally{
-		
-	}
-	}
+
 	}
 
-		
-
-
-	
-		
-
-
+}
